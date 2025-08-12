cf01487a1c88fb19ff02297503f20c6b654da5ed
moorabbit <moorabbit@proton.me>
2025-08-11 21:23:15 -0400

    [*] refactor

--- js/root.js
+++ js/root.js
@@ -1,32 +1,7 @@
 const kDOMParser = new DOMParser();
 
-class HTMLTemplatedElement extends HTMLElement {
-
-  constructor(componentTemplatesURL) {
-
-    super();
-
-    (async () => {
-
-      await fetch(componentTemplatesURL)
-      .then((res) => res.text())
-      .then((html) => kDOMParser.parseFromString(html, "text/html"))
-      .then((doc) => doc.querySelector("template").content)
-      .then((dom) => {
-        this.attachShadow({ mode: "open" });
-        this.shadowRoot.append(dom);
-      });
-
-    })();
-
-  }
-
-};
-
 export {
 
   kDOMParser,
-
-  HTMLTemplatedElement
 
 };
